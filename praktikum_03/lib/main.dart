67c568b43779b9d782b545e37431fe85667f7dda
maulanaryan59 <maulanaryan59@gmail.com>
2024-03-16 22:53:50 +0700

    first commit

--- praktikum_03/lib/main.dart
+++ praktikum_03/lib/main.dart
@@ -1,15 +1,25 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:praktikum_03/views/home_screen.dart';
+import '../../bloc/contact_cubit.dart';
 
-void main() => runApp(MyApp());
+void main() {
+  runApp(const MyApp());
+}
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(title: Text("Aplikasi Pertemuan 3")),
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(
+          create: (context) => ContactCubit(),
+        ),
+      ],
+      child: const MaterialApp(
+        home: HomeScreen(),
       ),
     );
   }
