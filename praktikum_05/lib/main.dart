549d03cdcb8d184cdbed32c616424311ac28c246
maulanaryan59 <maulanaryan59@gmail.com>
2024-03-22 15:41:03 +0700

    PPB-2324-2

---
+++ praktikum_05/lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:praktikum_05/home_page.dart';
+import 'package:praktikum_05/second_page.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(useMaterial3: false),
+      debugShowCheckedModeBanner: false,
+      //home: HomePage(),
+      initialRoute: '/',
+      routes: {
+        '/': (context) => const HomePage(),
+        '/second_page': (context) => const SecondPage(),
+      },
+    );
+  }
+}
